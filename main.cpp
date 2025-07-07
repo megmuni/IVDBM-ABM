52928438047ba05027a884da91455282fedf9357
megmuni <93480624+megmuni@users.noreply.github.com>
2025-07-07 10:34:26 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File: main.cpp
+ *
+ * File Contents: TODO 
+ *
+ * Author: alireza
+ * Contributors: Caroline Shung
+ *               Nuttiiya Seekhao
+ *               Kimberley Trickey
+ *
+ * Created on May 19, 2013, 7:34 AM
+ */
+/*****************************************************************************
+ ***  Copyright (c) 2013 by A. Najafi-Yazdi                                ***
+ *** This computer program is the property of Alireza Najafi-Yazd          ***
+ *** and may contain confidential trade secrets.                           ***
+ *** Use, examination, copying, transfer and disclosure to others,         ***
+ *** in whole or in part, are prohibited except with the express prior     ***
+ *** written consent of Alireza Najafi-Yazdi.                              ***
+ *****************************************************************************/  // TODO(Kim): Update the file comment once we figure out the copyright issues
+
+#include <cstdlib>
+#include <iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main() {
+    cout<<"Hello World!";
+    return 0;
+}
+
