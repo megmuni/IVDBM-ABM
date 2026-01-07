f0ea9c866a32ab0e81e2a5972dca670b167e884d
megmuni <meghanamunipalle@gmail.com>
2026-01-07 08:49:26 -0500

    Update Cell.cpp

--- src/Agent/Usr_Agents/Cell.cpp
+++ src/Agent/Usr_Agents/Cell.cpp
@@ -1463,7 +1463,7 @@ void Stem::differentiateStem(int number = 1, int agentType = progen) {
 
 	int in = this->index[read_t];
 	//float stemDiff = 0.5 + (Stem::differentiation[3] * meanTGF);
-	float stemDiff = 50; //testing
+	float stemDiff = 30; //testing
 	if (rollDice(stemDiff)) {
 		if (rollDice(70)) { // asymmetric differentiation
 			this->hatchnewcell(number, agentType); // only create new progenitor cell nearby
@@ -1486,21 +1486,24 @@ void Progen::differentiateProgen(int number = 1, int agentType = np) {
 	// np progenitor cells differentiate to the next stage, np cells
 
 	int in = this->index[read_t];
-	if (rollDice(70)) { // asymmetric differentiation
-		this->hatchnewcell(number, agentType); // only create new NP cell nearby
-	}
-	else { // symmetric differentiation
-		//Progen* temp = this;
-		//NP* dp2 = dynamic_cast<NP*>(temp); // dynamic cast to next derived class (cell type)
-		//if (dp2 == nullptr) {
-		//	cout << "Casting Failed" << endl;
-		//}
-		//else {
-		//	cout << "Casting Successful" << endl;
-		//}
-		Agent::agentPatchPtr[in].clearOccupied();
-		this->hatchnewcell(number, agentType, 1); // 'change' progenitor cell here to NP cell
-		this->hatchnewcell(number, agentType); // create new NP cell nearby
-		this->die(); // 'kill' current cell
+	float progenDiff = 30; //testing
+	if (rollDice(progenDiff)) {
+		if (rollDice(70)) { // asymmetric differentiation
+			this->hatchnewcell(number, agentType); // only create new NP cell nearby
+		}
+		else { // symmetric differentiation
+			//Progen* temp = this;
+			//NP* dp2 = dynamic_cast<NP*>(temp); // dynamic cast to next derived class (cell type)
+			//if (dp2 == nullptr) {
+			//	cout << "Casting Failed" << endl;
+			//}
+			//else {
+			//	cout << "Casting Successful" << endl;
+			//}
+			Agent::agentPatchPtr[in].clearOccupied();
+			this->hatchnewcell(number, agentType, 1); // 'change' progenitor cell here to NP cell
+			this->hatchnewcell(number, agentType); // create new NP cell nearby
+			this->die(); // 'kill' current cell
+		}
 	}
 }
