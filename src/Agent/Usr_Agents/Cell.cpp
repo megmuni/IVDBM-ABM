bc721bfb9dfc747cca19d60006bc9cf75b18a7f5
megmuni <meghanamunipalle@gmail.com>
2026-02-18 09:32:16 -0500

    Update Cell.cpp
    Added O2 consumption for stem cell class

--- src/Agent/Usr_Agents/Cell.cpp
+++ src/Agent/Usr_Agents/Cell.cpp
@@ -666,22 +666,9 @@ void Stem::stem_cellFunction() {
 	int in = this->index[read_t];
 	double hours = Agent::agentWorldPtr->reportHour();
 	int totaldamage = ((Agent::agentWorldPtr)->worldPatch)->numOfEachTypes[damage];
-
-	/* -------------------------------------------------------------------------- */
-	/*                                PROLIFERATION                               */
-	/* -------------------------------------------------------------------------- */
   	
-	// Stem cells in vitro (MODEL_SCAFFOLD) proliferate:
 	#ifdef MODEL_SCAFFOLD
 		in = this->index[read_t];
-		//cout << "[in] is " << in << endl; //debugging
-		//cout << "[read_t] is " << read_t << endl; //debugging
-		//if (Agent::agentPatchPtr[in].type[read_t] == CaAlg) {
-		//	cout << "agent patch type is " << Agent::agentPatchPtr[in].type[read_t] << endl; //added for debug
-		//}
-		//else {
-		//	//cout << "agent patch type is " << Agent::agentPatchPtr[in].type[read_t] << endl; //added for debug
-		//	cout << "agent patch type is invalid!" << endl;
 		//}
 		
 		if ((Agent::agentPatchPtr[in].type[read_t] == CaAlg) && (this->isProliferative(stem))) {
