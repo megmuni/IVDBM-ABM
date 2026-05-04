7d98040086528d796fa78815c2bf8f2a1aaa839d
megmuni <meghanamunipalle@gmail.com>
2026-05-04 13:46:35 -0400

    Renamed WHChemical class to Chemical

---
+++ src/FieldVariable/Usr_FieldVariables/Chemical.cpp
@@ -0,0 +1,20 @@
+/* 
+ * Chemical.cpp
+ * 
+ * File Contents: Contains Chemical class
+ *
+ * Author: Yvonna
+ * Contributors: Caroline Shung
+ *               Nuttiiya Seekhao
+ *               Kimberley Trickey
+ *				 Meghana Munipalle
+ */
+
+#include "Chemical.h"
+#include <iostream>
+
+using namespace std;
+
+Chemical::Chemical() {}
+Chemical::Chemical(int nx, int ny, int nz) {}
+Chemical::~Chemical() {}
