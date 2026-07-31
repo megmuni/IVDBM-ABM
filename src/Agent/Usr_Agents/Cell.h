6a250ad09f9c54725459926623ada8fa1177fd87
megmuni <meghanamunipalle@gmail.com>
2026-07-31 13:03:50 -0400

    Removed unused Stem::proliferation[1]
    This parameter dictated stem cell division check (every 24 hours) but that parameter is already captured in Cell::proliferation

--- src/Agent/Usr_Agents/Cell.h
+++ src/Agent/Usr_Agents/Cell.h
@@ -324,7 +324,7 @@ class Stem: public Cell {
   static float CollagenSynth[1];   // Parameters invloved in collagen synthesis in CaAlg Gel
   static float AggrecanSynth[1];   // Parameters invloved in aggrecan synthesis in CaAlg Gel
   static float ECMsynthesis[4]; // Parameters involved in ECM synthesis (baseline rates, hours between synth)
-  static float proliferation[5]; // Parameters involved in stem cell proliferation (coefficients for probabilistic differentiation
+  static float proliferation[4]; // Parameters involved in stem cell proliferation (coefficients for probabilistic differentiation
   static float differentiation[5]; // Parameters involved in stem cell differentiation
 
 protected:
